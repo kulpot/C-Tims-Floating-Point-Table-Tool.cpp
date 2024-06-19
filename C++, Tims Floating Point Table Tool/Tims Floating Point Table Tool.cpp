0dfedae01ee611677a975d5d58be9f3a13d1ec5c
Kulpot <kulpotwahu@gmail.com>
2024-06-20 02:07:34 +0800

    a

--- C++, Tims Floating Point Table Tool/Tims Floating Point Table Tool.cpp
+++ C++, Tims Floating Point Table Tool/Tims Floating Point Table Tool.cpp
@@ -1,3 +1,3 @@
 //C++, Tims Floating Point Table Tool   //ref link:https://www.youtube.com/watch?v=tz3rr8UB6sU&list=PLRwVmtr-pp04ljVWgUFlQoBl9tqTzFh2N&index=7
 
-//  1/2     /  1/4     /  1/16     
+//  1/2     |  1/4     |  1/16     /  1/32     / 1/64     
